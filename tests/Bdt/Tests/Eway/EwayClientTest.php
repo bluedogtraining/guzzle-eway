e5e0b6568f7940ee106ea46d91d4ce3f5a31742b
Johnson Page <jwpage@gmail.com>
2014-01-28 10:34:37 +1100

    Add test for non-numerical error codes.

--- tests/Bdt/Tests/Eway/EwayClientTest.php
+++ tests/Bdt/Tests/Eway/EwayClientTest.php
@@ -65,8 +65,25 @@ class EwayClientTest extends GuzzleTestCase
         $this->assertEquals('13', $response['trxnError']['code']);
         $this->assertEquals('Invalid Amount(Test CVN Gateway)', $response['trxnError']['message']);
         $this->assertFalse($response['trxnStatus']);
+    }
 
-
+    public function testSendPaymentFail2()
+    {
+        $this->setMockResponse($this->client, 'sendpayment_failure_2.txt');
+        $response = $this->client->getCommand('SendPayment', array(
+            'customerID'      => '87654321',
+            'totalAmount'     => '13',
+            'cardHoldersName' => 'Foo Bar',
+            'cardNumber'      => '4111111111111111',
+            'cardExpiryMonth' => '06',
+            'cardExpiryYear'  => '20',
+            'CVN'             => '123',
+        ))->execute();
+        $this->assertEquals(null, $response['trxnError']['code']);
+        $this->assertEquals('eWAY Error: Invalid Expiry Date. Your credit card has not been  billed for this transaction.',
+            $response['trxnError']['message']
+        );
+        $this->assertFalse($response['trxnStatus']);
     }
 
 }
